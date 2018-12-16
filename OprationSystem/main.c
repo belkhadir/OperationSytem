c52b3408e418bafd02ba52f2b7fdc5d20064b5bf
belkhadir <belkhadir.anas@gmail.com>
2018-12-16 22:07:00 +0100

    Initial Commit

---
+++ OprationSystem/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OprationSystem
+//
+//  Created by xxx on 12/16/18.
+//  Copyright © 2018 xxx. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
